f899c86e9ab11be7afcbbd57c20d08a7945db38e
Raquel Fernandes <94188045+fernandesraquel@users.noreply.github.com>
2023-05-25 16:45:26 -0300

    implementação US02 e US03 #45

---
+++ construct_/estoque/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('add_produto/', views.add_produto, name="add_produto"),
+    path('produto/<slug:slug>', views.produto, name="produto")
+    
+]
